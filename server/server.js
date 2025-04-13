acf9cbbf6c8f8511743d2ac269c80b329fc52740
Sergey Golubev <golubev.irk@gmail.com>
2025-04-13 13:05:57 +0200

    rebuild

--- server/server.js
+++ server/server.js
@@ -5,19 +5,21 @@ import { fileURLToPath } from 'url';
 
 dotenv.config();
 
-
 const PORT = process.env.PORT || 8080;
 const app = express();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
+// Serve static files from the client/dist folder
+app.use(express.static(path.resolve(__dirname, "../client/dist")));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+// Handle all other routes by serving the index.html file
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/dist/index.html"));
 });
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
